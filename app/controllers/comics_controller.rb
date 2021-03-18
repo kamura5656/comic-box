1ee2c2dc9276f8551b15cc9f033b14e1033ebc6c
kamura5656 <78400968+kamura5656@users.noreply.github.com>
2021-03-18 17:29:12 +0900

    漫画登録機能を実装

--- app/controllers/comics_controller.rb
+++ app/controllers/comics_controller.rb
@@ -1,4 +1,24 @@
 class ComicsController < ApplicationController
   def index
+    @comics = Comic.all.order('created_at DESC')
+  end
+
+  def new
+    @comic = Comic.new
+  end
+
+  def create
+    @comic = Comic.new(comic_params)
+    if @comic.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def comic_params
+    params.require(:comic).permit(:title, :author)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "comics#index"
+  resource :comics, only: [:new, :create]
 end
